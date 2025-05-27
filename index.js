43bbf6525760f8c60a72c20827bdb71ad944a3f6
Relizc <3464840574@qq.com>
2025-05-27 15:12:59 +0800

    stuff.js

--- index.js
+++ index.js
@@ -1,45 +1,141 @@
-const canvas = document.getElementById('canvas');
-    const ctx = canvas.getContext('2d');
 
 
-    // Rectangle properties
-    const rect = {
-      x: 100,
-      y: 100,
-      width: 200,
-      height: 150,
-      speed: 10
-    };
+class Game {
 
-    // Draw function
-    function draw() {
-      // Clear canvas
-      ctx.fillStyle = 'black';
-      ctx.fillRect(0, 0, canvas.width, canvas.height);
+  constructor() {
+    this.sprites = []
+    this.frameUpdate = true;
+    this.rq = 0
+    this.f = 0
+    this.hz = 0
+    this.keys = new Set();
 
-      // Draw rectangle
-      ctx.fillStyle = 'white';
-      ctx.fillRect(rect.x, rect.y, rect.width, rect.height);
+    const canvas = document.getElementById('canvas');
+    this.ctx = canvas.getContext('2d');
+    this.ctx.scale(2, 2)
+    
+  }
+
+  update() {
+    this.hz += 1
+
+    for (let sprites of this.sprites) {
+      sprites.update()
     }
+  }
 
-    // Handle keyboard input
-    window.addEventListener('keydown', (e) => {
-      switch (e.key) {
-        case 'ArrowUp':
-          rect.y -= rect.speed;
-          break;
-        case 'ArrowDown':
-          rect.y += rect.speed;
-          break;
-        case 'ArrowLeft':
-          rect.x -= rect.speed;
-          break;
-        case 'ArrowRight':
-          rect.x += rect.speed;
-          break;
+  queueRender() {
+    this.frameUpdate = true
+  }
+
+  addSprite(sprite) {
+    
+    this.sprites.push(sprite)
+  }
+
+  animate() {
+    this.rq += 1
+    this.draw()
+
+    
+
+    requestAnimationFrame(() => {this.animate()})
+  }
+
+  draw() {
+
+    if (!this.frameUpdate) return;
+    this.f += 1
+    this.frameUpdate = false
+
+    this.ctx.fillStyle = 'black';
+    this.ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    for (let sprites of this.sprites) {
+      sprites.draw(this.ctx)
+      if (sprites.drawHitbox) {
+        sprites._drawHitbox(this.ctx)
       }
-      draw(); // Redraw after moving
-    });
+    }
+  }
 
-    // Initial draw
-    draw();
+  startShowingFps() {
+    setInterval(() => {
+      console.log("rq: " + this.rq + " f: " + this.f, " " + this.hz + "Hz\nkeys: " + Array.from(this.keys).join(", "))
+      this.f = 0
+      this.rq = 0
+      this.hz = 0
+    }, 1000)
+  }
+
+  startTicking() {
+    setInterval(() => {
+      this.update()
+    }, 20) // 50 tps
+  }
+
+}
+
+
+
+class Sprite {
+
+  constructor(hitbox) {
+    this.hitbox = hitbox
+    this.showHitbox = false;
+    this.position = [0, 0]
+
+    Game.instance.addSprite(this)
+    Game.instance.queueRender()
+  }
+
+  draw(ctx) {
+  
+  }
+
+  update() {
+    
+  }
+
+  _drawHitbox(ctx) {
+    console.log(1)
+    ctx.strokeStyle = "#00FF00"
+    ctx.lineWidth = 1
+    ctx.strokeRect(this.position[0], this.position[1], this.position[0] + this.hitbox[0], this.position[1] + this.hitbox[1])
+  }
+
+}
+
+class MomentumObject extends Sprite {
+
+  constructor(hitbox) {
+    super(hitbox)
+
+    this.force = [0, 0]
+    this.velocity = [0, 0]
+    this.acceleration = [0, 0]
+    this.mass = 1
+  }
+
+  update() {
+
+  }
+
+  draw(ctx) {
+    console.log("rendering: " + this)
+  }
+
+}
+
+game = new Game()
+Game.instance = game
+Game.instance.animate()
+Game.instance.startShowingFps()
+Game.instance.startTicking()
+
+sprite = new MomentumObject([8, 15]);
+sprite.drawHitbox = true
+
+window.addEventListener('keydown', (e) => Game.instance.keys.add(e.key));
+window.addEventListener('keyup', (e) => Game.instance.keys.delete(e.key));
+
